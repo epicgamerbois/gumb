0687a83c846f3cc6ea3bc302b2231984c26dcfdf
epicgamerbois <41995108+epicgamerbois@users.noreply.github.com>
2018-08-01 08:17:16 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,29 @@ client.on('message', message => {
   	}
 });
 
+client.on("message", async (message) => {
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+	
+	let command = message.content.split(" ")[0];
+	command = command.slice(prefix.length);
+	
+	let args = message.content.split(" ").slice(1);
+	
+	if (command === "ping") {
+		message.channel.send(`Pong! Time took: ${Date.now() - message.createdTimestamp} ms`);
+	} else
+
+	if (command === "say") {
+		message.delete()
+        const embed = new Discord.RichEmbed()
+		.setColor(0x954D23)
+		.setDescription(message.author.username + " says: " + args.join(" "));
+		message.channel.send({embed})
+	} 
+
+	
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
